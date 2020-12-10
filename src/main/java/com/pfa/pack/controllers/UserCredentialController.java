7543aa531259e52ea0e1ecc3e4123118a9c0277d
SelimHorri <horri.slim@gmail.com>
2020-12-10 22:50:15 +0100

    add javadoc under /docs

--- src/main/java/com/pfa/pack/controllers/UserCredentialController.java
+++ src/main/java/com/pfa/pack/controllers/UserCredentialController.java
@@ -1,7 +1,5 @@
 package com.pfa.pack.controllers;
 
-import java.util.Optional;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
