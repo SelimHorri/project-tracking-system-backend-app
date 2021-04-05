71890cba0a9dc02e825b898f9a60a5f844c1ac07
SelimHorri <horri.slim@gmail.com>
2021-04-05 22:58:16 +0100

    add cors config & test employee findAll

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { EmployeesCollection } from './model/dto/employees-collection';
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from './service/employee.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  
+  constructor(private employeeService: EmployeeService) {
+    
+  }
+  
+  ngOnInit() {
+    this.findAll();
+  }
+  
+  public findAll(): void {
+    this.employeeService.findAll().subscribe(
+      (response: EmployeesCollection) => {
+        response.employees.forEach((e) => {
+          console.log(e.firstName + " " + e.lastName + " : " + e.email);
+        });
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error.message);
+        alert(error.message);
+      }
+    );
+  }
+  
+  
+  
 }
+
+
+
--- src/main/java/com/pfa/pack/configs/cors/CorsConfig.java
+++ src/main/java/com/pfa/pack/configs/cors/CorsConfig.java
@@ -1,5 +1,7 @@
 package com.pfa.pack.configs.cors;
 
+import java.util.Arrays;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +13,7 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 public class CorsConfig {
 	
+	// public static final String allowedOrigin = "http://localhost:4200";
 	private static final Logger logger = LoggerFactory.getLogger(CorsConfig.class);
 	
 	static {
@@ -18,21 +21,21 @@ public class CorsConfig {
 	}
 	
 	@Bean
-    public CorsFilter corsFilter() {
+	public CorsFilter corsFilter() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://localhost:4200/"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+															"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+															"Access-Control-Request-Method", "Access-Control-Request-Headers"));
+		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
+															"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("OPTIONS");
-        config.addAllowedMethod("GET");
-        config.addAllowedMethod("POST");
-        config.addAllowedMethod("PUT");
-        config.addAllowedMethod("DELETE");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+		return new CorsFilter(urlBasedCorsConfigurationSource);
+	}
 	
 	
 
