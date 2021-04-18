1c299711432eb5b747d785637820bc5ddeab4d5a
Ramazan Sakin <ramazansakin63@gmail.com>
2021-04-18 12:58:22 +0300

    Because of H2 projections problem, I changed the repo function interface as getting just employee ids and then convert.

--- src/main/java/com/pfa/pack/converters/EmployeeAssignedProjectConverter.java
+++ src/main/java/com/pfa/pack/converters/EmployeeAssignedProjectConverter.java
@@ -1,31 +1,23 @@
 package com.pfa.pack.converters;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-
+import com.pfa.pack.models.dto.EmployeeAssignedProjectDto;
+import com.pfa.pack.models.entities.Employee;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
-import com.pfa.pack.models.dto.EmployeeAssignedProject;
-import com.pfa.pack.models.dto.EmployeeAssignedProjectDto;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
-public class EmployeeAssignedProjectConverter implements Converter<Set<EmployeeAssignedProject>, List<EmployeeAssignedProjectDto>> {
+public class EmployeeAssignedProjectConverter implements Converter<List<Employee>, List<EmployeeAssignedProjectDto>> {
 	
 	@Override
-	public List<EmployeeAssignedProjectDto> convert(final Set<EmployeeAssignedProject> source) {
-		
+	public List<EmployeeAssignedProjectDto> convert(final List<Employee> source) {
 		final List<EmployeeAssignedProjectDto> emps = new ArrayList<>();
-		source.forEach((c) -> {
-			emps.add(new EmployeeAssignedProjectDto(c.getEmployeeId(), c.getFirstName() + " " + c.getLastName(), (c.getVerif() == 1) ? true : false ));
-		});
-		
+		source.forEach(c -> emps.add(new EmployeeAssignedProjectDto(c.getEmployeeId(), c.getFirstName() + " " + c.getLastName(), false )));
 		return emps;
 	}
-	
-	
-	
+
 }
 
 
